16a893283fcaae75733dd3b120fbd2f28e7e5129
Stas Voievodin <vstasv@i.ua>
2023-04-20 15:49:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,39 +11,38 @@ const countryInfo = document.querySelector('.country-info');
 searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
-    if (e.target.value.trim() !== '') {
-        fetchCountries(e.target.value.trim()).then(data => {
-            if (data.length > 10) {
-                Notify.info('Too many matches found. Please enter a more specific name.');
-            };
-            if (data.length <= 10) {
-                countryList.innerHTML = countryListMarkup(data);
-                countryInfo.innerHTML = "";
-            }
-            if (data.length === 1) {
-                countryInfo.innerHTML = countryInfoMarkup(data);
-                countryList.innerHTML = "";
-            }
-            console.log(data);
-        }).catch(err => {
-            countryInfo.innerHTML = "";
-            countryList.innerHTML = "";
-            console.log(err);
-            Notify.failure('Oops, there is no country with that name');
-        });
-    }
+  if (e.target.value.trim() !== '') {
+    fetchCountries(e.target.value.trim()).then(data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      };
+      if (data.length <= 10) {
+        countryList.innerHTML = countryListMarkup(data);
+        countryInfo.innerHTML = "";
+      }
+      if (data.length === 1) {
+        countryInfo.innerHTML = countryInfoMarkup(data);
+        countryList.innerHTML = "";
+      }
+      console.log(data);
+    }).catch(err => {
+      console.log(err);
+      Notify.failure('Oops, there is no country with that name');
+    });
+  } countryInfo.innerHTML = "";
+  countryList.innerHTML = "";
 };
 
 function countryListMarkup(arr) {
-    return arr.map(({ flags: { svg, alt }, name: { official } }) =>
-        `<li style="list-style: none;">
+  return arr.map(({ flags: { svg, alt }, name: { official } }) =>
+    `<li style="list-style: none;">
         <img src="${svg}" alt="${alt}" height = "25" width="40" />
       <span style="font-size: 18px;">${official}</span></li>`).join('')
 }
 
 function countryInfoMarkup(arr) {
-    return arr.map(({ flags: { svg, alt }, name: { official }, capital, languages, population }) =>
-        `<ul style="list-style: none;">
+  return arr.map(({ flags: { svg, alt }, name: { official }, capital, languages, population }) =>
+    `<ul style="list-style: none;">
         <li>
           <img style="display: inline-block;" src="${svg}" alt="${alt}" height="25" />
           <h1 style="font-size: 42px; display: inline-block;">${official}</h1>
